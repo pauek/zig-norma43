0b9ba72478a1e41f8f3f43638274216094057136
Pau Fernández <pau.fernandez@upc.edu>
2022-09-01 15:35:19 +0200

    Refactor++

--- src/extracto.zig
+++ src/extracto.zig
@@ -6,17 +6,38 @@ const Cuenta = @import("./cuenta.zig").Cuenta;
 pub const ExtractoBancario = struct {
     const Self = @This();
 
+    allocator: std.mem.Allocator,
     cuentas: std.ArrayList(Cuenta),
     cuentaActual: ?*Cuenta,
 
     pub fn init(allocator: std.mem.Allocator) Self {
         return Self{
+            .allocator = allocator,
             .cuentas = std.ArrayList(Cuenta).init(allocator),
             .cuentaActual = null,
         };
     }
 
-    pub fn parseRegister(self: *ExtractoBancario, line: *const [82]u8, allocator: std.mem.Allocator) !void {
+    pub fn readFrom(self: *Self, filename: []const u8) !void {
+        var file = try std.fs.cwd().openFile(filename, .{});
+        defer file.close();
+
+        var buf_reader = std.io.bufferedReader(file.reader());
+        var in_stream = buf_reader.reader();
+        var buf: [82]u8 = undefined;
+
+        while (true) {
+            var nread = try in_stream.read(&buf);
+            if (nread == 0) {
+                break;
+            } else if (nread != 82) {
+                unreachable;
+            }
+            try self.parseRegister(&buf, self.allocator);
+        }
+    }
+
+    pub fn parseRegister(self: *Self, line: *const [82]u8, allocator: std.mem.Allocator) !void {
         var code = util.code(line);
         if (code == 11) {
             var nueva: *Cuenta = try self.cuentas.addOne();
@@ -32,6 +53,13 @@ pub const ExtractoBancario = struct {
             }
         } else {
             // util.print("Line: {x}\n", .{std.fmt.fmtSliceHexLower(line)});
+        }
+    }
+
+    pub fn dump(self: *const Self) !void {
+        for (self.cuentas.items) |cuenta| {
+            cuenta.print();
+            util.print("\n", .{});
         }
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -3,30 +3,10 @@ const util = @import("./util.zig");
 const ExtractoBancario = @import("./extracto.zig").ExtractoBancario;
 
 pub fn main() anyerror!void {
-    var file = try std.fs.cwd().openFile("iber-caja-2022.aeb43", .{});
-    defer file.close();
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
     var extracto = ExtractoBancario.init(allocator);
-
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
-    var buf: [82]u8 = undefined;
-
-    while (true) {
-        var nread = try in_stream.read(&buf);
-        if (nread == 0) {
-            break;
-        } else if (nread != 82) {
-            unreachable;
-        }
-        try extracto.parseRegister(&buf, allocator);
-    }
-
-    for (extracto.cuentas.items) |cuenta| {
-        cuenta.print();
-        util.print("\n", .{});
-    }
+    try extracto.readFrom("iber-caja-2022.aeb43");
+    try extracto.dump();
 }
